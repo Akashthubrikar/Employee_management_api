e1adb2ae025342be7b8c93e0a9065d3bde096410
Akashthubrikar <akashthubrikar@example.com>
2024-10-23 18:11:38 +0530

    added code in app.py file

--- app.py
+++ app.py
@@ -15,3 +15,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
